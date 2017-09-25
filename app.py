6c8f7591b4c0a8f759278cd7cbdfed8a92a88234
Todd Riemenschneider <twr14152@gmail.com>
2017-09-25 14:55:26 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
+app = Flask(__name__)
+
+app.secret_key = "The most secret key ever"
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first.")
+            return redirect(url_for("login"))
+    return wrap
+
+
+@app.route("/")
+@login_required
+def home():
+#    return "Hello World!"
+    return render_template("index.html")
+     
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html") # render a template
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            error = "Invalid credentials, please try again."
+        else:
+            session["logged_in"] = True
+            flash("You were just logged in")
+            return redirect(url_for("home"))
+    return render_template("login.html", error=error)
+
+@app.route("/logout")
+@login_required
+def logout():
+    session.pop("logged_in", None)
+    flash("You were just logged out") 
+    return redirect(url_for("welcome"))
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
