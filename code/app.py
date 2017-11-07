4430d044717d43df8293174a32e833d20d517bab
Todd Riemenschneider <twr14152@gmail.com>
2017-11-07 19:16:41 +0000

    Update app.py

--- code/app.py
+++ code/app.py
@@ -4,14 +4,25 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {"student": name}
+items = []
 
-# http://x.x.x.x:5000/student/Chester
-# flask_restful module removes need for creating decorators
-#
-api.add_resource(Student, "/student/<string:name>")
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item["name"] == name:
+                return item
+        return {"item": None}, 404 #error code 404 NOT FOUND
+
+
+    def post(self, name):
+        item = {"name": name, "price": 12.00}
+        items.append(item)
+        return item, 201 # Code 201 is for created
+
+
+
+api.add_resource(Item, "/item/<string:name>") 
 
 
 app.run(host="0.0.0.0", port=5000)
