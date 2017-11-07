ba7c60d8d77624f27b8c300c729ecade3e54b80e
Todd Riemenschneider <twr14152@gmail.com>
2017-11-07 18:26:03 +0000

    Create app.py

---
+++ code/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Student(Resource):
+    def get(self, name):
+        return {"student": name}
+
+# http://127.0.0.1:5000/student/Chester
+api.add_resource(Student, "/student/<string:name>")
+
+
+app.run(host="0.0.0.0", port=5000)
+~                                      
