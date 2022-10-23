bd35bbfb535150bfc1b8a27465c402deca2e7d92
Todd Riemenschneider <twr14152@gmail.com>
2022-10-23 15:11:46 -0400

    Create flask_Sqllite.py

---
+++ projectBlue/flask_Sqllite.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import sqlite3 as sql
+from sqlite3 import Error
+app = Flask(__name__)
+
+
+conn = sql.connect('database.db')
+print("DB openned successfully")
+
+conn.execute("CREATE TABLE IF NOT EXISTS user_data (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEST, email TEXT, password TEXT)")
+print("Table created successfully")
+conn.close()
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+@app.route('/add')
+def add():
+   return render_template('add_user_form.html')
+
+@app.route('/add_user',methods = ['POST', 'GET'])
+def add_user():
+   if request.method == 'POST':
+      try:
+      #   id = request.form['id']
+         first_name = request.form['first_name']
+         last_name = request.form['last_name']
+         email = request.form['email']
+         password = request.form['password']
+         operational_cost = request.form['operational_cost']
+
+
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO user_data (first_name, last_name, email, password, operational_cost) VALUES (?,?,?,?,?)",(first_name, last_name, email, password, operational_cost))
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+
+
+@app.route('/list')
+def list():
+    try:
+        con = sql.connect("database.db")
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM user_data ORDER BY id ASC")
+        rows = cur.fetchall();
+    except Error as e:
+        rows = e
+    finally:
+        return render_template("list.html",rows = rows)
+
