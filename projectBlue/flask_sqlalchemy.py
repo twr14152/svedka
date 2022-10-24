05aa145726c9f3ea867d32af7ca26afd3892426e
Todd Riemenschneider <twr14152@gmail.com>
2022-10-24 07:16:39 -0400

    Update flask_sqlalchemy.py

--- projectBlue/flask_sqlalchemy.py
+++ projectBlue/flask_sqlalchemy.py
@@ -1,1 +1,25 @@
-tbd
+#adding sqlalchemy support
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+#
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'mysupersecretkey'
+
+#
+app.config['SQLACHEMY_DATABASE_URL'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+#toolbar = DebugToolbarExtension(app)
+
+
+class userInfo(db.Model):
+    __tablename__ = 'userData'
+    id = db.Column(db.Integer, primary_key = True, next = True)
