668e2ab290156c5544d40ddf97d68bf1462a31f8
Todd Riemenschneider <twr14152@gmail.com>
2025-03-22 11:59:26 -0400

    Create create_user_accts.py

---
+++ prototype/create_user_accts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sqlite3 as sql
+from werkzeug.security import generate_password_hash
+
+
+email = input("Enter email: ")
+password = input("Enter password: ")
+admin = int(input("Is admin yes = 1 or no = 0: "))
+
+hashed_password = generate_password_hash(password)
+print(hashed_password)
+
+
+con = sql.connect("users.db")
+cur = con.cursor()
+# Assuming your table 'User' has columns 'email', 'password', 'is_admin'
+cur.execute("INSERT INTO Users (username, password, is_admin) VALUES (?, ?, ?)", (email, hashed_password, admin))
+con.commit()
+con.close(
